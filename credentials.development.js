2c994d4785df8c842d448abd6a78332c2aeb1d83
O/I <65306934+AF0577824641@users.noreply.github.com>
2025-05-14 13:35:20 -0400

    Updated production credentials

--- credentials.development.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  cookieSecret: process.env.COOKIESECRET || "MysuperSecretCookieSecret",
-  postgres: {
-    connectionString:
-      process.env.DBCONNECTIONSTRING ||
-      "postgresql://adonisfuentes:mypostgresql@localhost:5432/my_first_db",
-  },
-};
